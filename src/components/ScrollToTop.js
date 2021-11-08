6c71a68ec3f772215d4087e80ee079f74b042ddb
TingYunCho <a10060790121@hotmail.com>
2021-11-08 22:33:24 +0800

    scroll to top modification

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import { withRouter } from 'react-router-dom'
+import React from 'react'
+import { withRouter } from 'react-router-dom'
 
 // // 頁面切換時要用捲軸讓頁面回到最上方
-// class ScrollToTop extends React.Component {
-//   componentDidUpdate(prevProps) {
-//     if (this.props.location !== prevProps.location) {
-//       window.scrollTo(0, 0)
-//     }
-//   }
+class ScrollToTop extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0)
+    }
+  }
 
-//   render() {
-//     return this.props.children
-//   }
-// }
+  render() {
+    return this.props.children
+  }
+}
 
-// export default withRouter(ScrollToTop)
+export default withRouter(ScrollToTop)
 
 // import React, { useState } from 'react'
 // import { FaArrowCircleUp } from 'react-icons/fa'
